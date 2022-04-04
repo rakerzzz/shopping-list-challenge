1e31a8786b2a808e55d22b588a029c68ad1b1b81
Sherwin Labrador <LabradorSherwinReyes@gmail.com>
2022-04-04 21:14:25 +0800

    Update script.js
    adding comments based on what challenge we're going to do.

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 var listItem = document.getElementsByTagName("li");
 
-
+//1. If you click on the list item, it toggles the .done  class on and off.
 ul.addEventListener("click", function(e){
 		e.target.classList.toggle("done");
 		
 })
+
+//2. Add buttons next to each list item to delete the item when clicked on its corresponding delete button.
 
 for (i=0; i<listItem.length; i++){
 	var btn=document.createElement("button");
@@ -31,6 +33,7 @@ function createListElement() {
 	ul.appendChild(li);
 	input.value = "";
 
+	//3. BONUS: When adding a new list item, it automatically adds the delete button next to it (hint: be sure to check if new items are clickable too!)
 	var btn = document.createElement("button");
 	btn.appendChild(document.createTextNode("Delete!"));
 	li.appendChild(btn);
